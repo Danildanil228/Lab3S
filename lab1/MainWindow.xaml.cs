4cc40db92bd7b7aefbb119786ac45da87204e68b
Danil <Danil@DESKTOPDEWPEEK>
2024-02-19 19:58:01 +0300

    Добавьте файлы проекта.

---
+++ lab1/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Windows;
+
+namespace MoneyApp
+{
+    public partial class MainWindow : Window
+    {
+        private Money currentMoney;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            currentMoney = new Money();
+            UpdateMoneyDisplay();
+        }
+
+        private void UpdateMoneyDisplay()
+        {
+            txtMoney.Text = currentMoney.ToString();
+        }
+
+        private void AddMoney_Click(object sender, RoutedEventArgs e)
+        {
+            if (decimal.TryParse(txtAddAmount.Text, out decimal amount))
+            {
+                currentMoney.AddMoney(new Money(new Dictionary<decimal, int> { { amount, 1 } }));
+                UpdateMoneyDisplay();
+            }
+            else
+            {
+                MessageBox.Show("Неверный ввод для добавления денег.");
+            }
+        }
+
+        private void SubtractMoney_Click(object sender, RoutedEventArgs e)
+        {
+            if (decimal.TryParse(txtSubtractAmount.Text, out decimal amount))
+            {
+                var toSubtract = new Money(new Dictionary<decimal, int> { { amount, 1 } });
+                try
+                {
+                    currentMoney.SubtractMoney(toSubtract);
+                    UpdateMoneyDisplay();
+                }
+                catch (InvalidOperationException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Неверный ввод для вычитания суммы.");
+            }
+        }
+
+        private void DivideMoney_Click(object sender, RoutedEventArgs e)
+        {
+            if (decimal.TryParse(txtDivideBy.Text, out decimal divisor) && divisor != 0)
+            {
+                currentMoney.DivideBy(divisor);
+                UpdateMoneyDisplay();
+            }
+            else
+            {
+                MessageBox.Show("Неверный ввод для деления суммы.");
+            }
+        }
+
+        private void MultiplyMoney_Click(object sender, RoutedEventArgs e)
+        {
+            if (decimal.TryParse(txtMultiplyBy.Text, out decimal multiplier))
+            {
+                currentMoney.MultiplyBy(multiplier);
+                UpdateMoneyDisplay();
+            }
+            else
+            {
+                MessageBox.Show("Неверный ввод для умножения суммы.");
+            }
+        }
+
+        private void CompareMoney_Click(object sender, RoutedEventArgs e)
+        {
+            if (decimal.TryParse(txtCompareAmount.Text, out decimal compareAmount))
+            {
+                var compareMoney = new Money(new Dictionary<decimal, int> { { compareAmount, 1 } });
+                int comparisonResult = currentMoney.CompareTo(compareMoney);
+                if (comparisonResult > 0)
+                {
+                    txtComparisonResult.Text = "Текущая сумма больше.";
+                }
+                else if (comparisonResult < 0)
+                {
+                    txtComparisonResult.Text = "Текущая сумма меньше.";
+                }
+                else
+                {
+                    txtComparisonResult.Text = "Текущая сумма равны.";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Неверный ввод для сравнения сумм.");
+            }
+        }
+    }
+}
---
+++ lab1/Money.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MoneyApp
+{
+    public class Money
+    {
+        private Dictionary<decimal, int> _denominations;
+
+        public Money()
+        {
+            _denominations = new Dictionary<decimal, int>
+            {
+                {0.01m, 0}, {0.05m, 0}, {0.1m, 0}, {0.5m, 0},
+                {1m, 0}, {2m, 0}, {5m, 0}, {10m, 0}, {50m, 0},
+                {100m, 0}, {500m, 0}, {1000m, 0}, {5000m, 0}
+            };
+        }
+
+        public Money(Dictionary<decimal, int> denominations)
+        {
+            _denominations = denominations;
+        }
+
+        public void AddMoney(Money money)
+        {
+            foreach (var kvp in money._denominations)
+            {
+                _denominations[kvp.Key] += kvp.Value;
+            }
+        }
+
+        public void SubtractMoney(Money money)
+        {
+            foreach (var kvp in money._denominations)
+            {
+                _denominations[kvp.Key] -= kvp.Value;
+                if (_denominations[kvp.Key] < 0)
+                {
+                    throw new InvalidOperationException("Недостаточно средств для вычета.");
+                }
+            }
+        }
+
+        public void DivideBy(decimal divisor)
+        {
+            foreach (var kvp in _denominations)
+            {
+                _denominations[kvp.Key] = (int)Math.Round(kvp.Value / divisor);
+            }
+        }
+
+        public void MultiplyBy(decimal multiplier)
+        {
+            foreach (var kvp in _denominations)
+            {
+                _denominations[kvp.Key] = (int)Math.Round(kvp.Value * multiplier);
+            }
+        }
+
+        public decimal TotalAmount()
+        {
+            decimal total = 0;
+            foreach (var kvp in _denominations)
+            {
+                total += kvp.Key * kvp.Value;
+            }
+            return total;
+        }
+
+        public int CompareTo(Money other)
+        {
+            return TotalAmount().CompareTo(other.TotalAmount());
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            foreach (var kvp in _denominations)
+            {
+                if (kvp.Value > 0)
+                {
+                    result += $"{kvp.Value}x{kvp.Key} ";
+                }
+            }
+            return result.Trim();
+        }
+    }
+}
